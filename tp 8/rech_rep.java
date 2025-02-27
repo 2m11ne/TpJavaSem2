147a8e550cffc84cf74b71ed3f2383c20663ae17
2m11ne <aminemb200@gmail.com>
2025-02-27 21:28:51 +0100

    rech_rep.java

--- tp 8/rech_rep.java
+++ tp 8/rech_rep.java
@@ -1,13 +1,9 @@
 import java.util.Scanner;
-
 public class rech_rep {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String Texte,mot1,mot2,x;
-        
         int l,p;
-        //StringBuffer texte;
-
         System.out.println("donner un chaine de charactere");
         Texte=sc.nextLine();
         System.out.println("donner mot1");
@@ -16,16 +12,12 @@ public class rech_rep {
         mot2=sc.nextLine();
         if (Texte.indexOf(mot1)==-1) {
             System.out.println(mot1+" n'existe pas dans le texte");
-
-            
         }
         else{
             l=mot1.length();
                 p=Texte.indexOf(mot1);
                 x=Texte.replace(mot1, mot2);
-
                 System.out.println("le nouveau texte est"+ x);
-               
         }
  
     }
